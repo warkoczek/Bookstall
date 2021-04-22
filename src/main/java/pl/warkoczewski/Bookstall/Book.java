76c8dbda7e295e2da8c86280e895cfe927b3ca36
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-04-22 12:38:41 +0200

    catalogService bean

---
+++ src/main/java/pl/warkoczewski/Bookstall/Book.java
@@ -0,0 +1,45 @@
+package pl.warkoczewski.Bookstall;
+
+public class Book {
+
+    private Long id;
+    private String title;
+    private String author;
+    private Integer year;
+
+    public Book(Long id, String title, String author, Integer year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ src/test/java/pl/warkoczewski/Bookstall/BookstallOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.warkoczewski.Bookstall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstallOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
