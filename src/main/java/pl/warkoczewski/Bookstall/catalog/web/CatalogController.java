4b265a734335052e2919cc7c600afa095fcff2ef
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-05-06 10:01:10 +0200

    CatalogController RestCreateBookCommand validator

--- src/main/java/pl/warkoczewski/Bookstall/catalog/web/CatalogController.java
+++ src/main/java/pl/warkoczewski/Bookstall/catalog/web/CatalogController.java
@@ -10,6 +10,10 @@ import pl.warkoczewski.Bookstall.catalog.application.port.CatalogUseCase;
 import pl.warkoczewski.Bookstall.catalog.application.port.CatalogUseCase.CreateBookCommand;
 import pl.warkoczewski.Bookstall.catalog.domain.Book;
 
+import javax.validation.Valid;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.List;
@@ -44,7 +48,7 @@ public class CatalogController {
                 .orElse(ResponseEntity.notFound().build());
     }
     @PostMapping
-    public ResponseEntity<?> addBook(@RequestBody RestCreateBookCommand command){
+    public ResponseEntity<?> addBook(@Valid @RequestBody RestCreateBookCommand command){
         Book book = catalog.addBook(command.toCommand());
         return ResponseEntity.created(createBookUri(book)).build();
     }
@@ -60,9 +64,14 @@ public class CatalogController {
     }
     @Data
     private static class RestCreateBookCommand{
+        @NotBlank
         private String title;
+        @NotBlank
         private String author;
+        @NotNull
         private Integer year;
+        @NotNull
+        @DecimalMin("0.00")
         private BigDecimal price;
 
         CreateBookCommand toCommand(){
