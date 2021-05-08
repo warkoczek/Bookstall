c772012a0834f965abf44307de18bdd0d863d907
warkoczek <53393030+warkoczek@users.noreply.github.com>
2021-05-08 11:28:39 +0200

    MemoryUploadRepository

--- src/main/java/pl/warkoczewski/Bookstall/upload/application/UploadService.java
+++ src/main/java/pl/warkoczewski/Bookstall/upload/application/UploadService.java
@@ -1,36 +1,37 @@
 package pl.warkoczewski.Bookstall.upload.application;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 import pl.warkoczewski.Bookstall.upload.application.port.UploadUseCase;
 import pl.warkoczewski.Bookstall.upload.domain.Upload;
+import pl.warkoczewski.Bookstall.upload.domain.UploadRepository;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@RequiredArgsConstructor
 public class UploadService implements UploadUseCase {
 
-    private Map<String, Upload> storage = new ConcurrentHashMap<>();
+    private final UploadRepository uploadRepository;
+
+    @Override
+    public Optional<Upload> getById(String id) {
+        return uploadRepository.findById(id);
+    }
 
     @Override
     public Upload save(SaveUploadCommand command) {
         String newId = RandomStringUtils.randomAlphanumeric(8).toLowerCase();
         Upload upload = new Upload(newId, command.getFile(), command.getContentType(),  command.getFilename(), LocalDateTime.now());
-        storage.put(upload.getId(), upload);
+        uploadRepository.save(upload);
         System.out.println("Upload saved: " + upload.getFilename() + " with newId " + newId);
         return upload;
     }
 
     @Override
-    public Optional<Upload> getById(String id) {
-        return Optional.of(storage.get(id));
-    }
-
-    @Override
     public void removeById(String id) {
-        storage.remove(id);
+        uploadRepository.removeById(id);
     }
 }
--- src/main/java/pl/warkoczewski/Bookstall/upload/infrastructure/MemoryUploadRepository.java
+++ src/main/java/pl/warkoczewski/Bookstall/upload/infrastructure/MemoryUploadRepository.java
@@ -1,6 +1,29 @@
 package pl.warkoczewski.Bookstall.upload.infrastructure;
 
+import pl.warkoczewski.Bookstall.upload.domain.Upload;
 import pl.warkoczewski.Bookstall.upload.domain.UploadRepository;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class MemoryUploadRepository implements UploadRepository {
+
+    private Map<String, Upload> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public Optional<Upload> findById(String id){
+        return Optional.ofNullable(storage.get(id));
+    }
+
+    @Override
+    public Upload save(Upload upload) {
+        storage.put(upload.getId(), upload);
+        return upload;
+    }
+
+    @Override
+    public void removeById(String id) {
+        storage.remove(id);
+    }
 }
